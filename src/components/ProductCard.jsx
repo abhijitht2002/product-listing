39527fa7b8314b16f3a11005a91f27b6df14d6b5
abhijith <abhijitht210@gmail.com>
2025-10-05 21:03:03 +0530

    updated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ function ProductCard({ product }) {
       <h1 className='text-xs uppercase text-gray-400 tracking-wide mb-1'>{product.category}</h1>
       <h2 className='text-base font-semibold text-gray-800 mb-1'>{product.name}</h2>
       <span className="text-sm text-gray-600 mb-1">{product.rating}</span>
-      <p className='font-semibold mb-3'>{product.price}</p>
+      <p className='font-semibold mb-3'>₹{product.price}</p>
       <button onClick={() => console.log(`item added to cart: ${product.name}`)} className='w-full border border-gray-500 text-black py-2 rounded-md hover:bg-blue-500 hover:text-white transition-colors duration-300'>
         Add to cart
       </button>
